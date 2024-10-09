3ef6fd5bcae801b5eda20a49d78df3625e62ea5c
vvtruong27 <vvtruong936@gmail.com>
2024-10-09 13:29:58 +0700

    fix database

--- database/migrations/2024_09_22_210205_create_visitors_table.php
+++ database/migrations/2024_09_22_210205_create_visitors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('visitors', function (Blueprint $table) {
+        Schema::create('Visitors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('phone');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('visitors');
+        Schema::dropIfExists('Visitors');
     }
 };
