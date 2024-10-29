55e272de148afa29d3be75956ed0fa76ae2edd2b
Max <mcdungkp@gmail.com>
2024-10-29 10:58:58 +0700

    update form input information

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>{{ config('app.name', 'omiCode Payment Gate') }}</title>
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
